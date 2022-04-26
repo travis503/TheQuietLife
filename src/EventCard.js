ad7541adaa579436241e34917a252c8c3fa22983
Travis Morse <travis503@gmail.com>
2022-04-26 02:35:13 -0500

    Code cleanup

--- src/EventCard.js
+++ src/EventCard.js
@@ -3,32 +3,9 @@ import DisplayModal from './DisplayModal.js';
 
 export default function EventCard(props) {
 
-  // const [eventModal, setEventModal] = useState(false);
-
   var openModal = () => {
-    console.log('Clicked on openModal!');
     props.setOpenModal(true);
-    console.log('OpenModal is now: ' + props.openModal);
   }
-
-  // var closeModal = () => {
-  //   setEventModal(false);
-  // }
-
-  // function EventModal(props) {
-  //   if (eventModal) {
-  //     return (
-  //       <div className="modal">
-  //         <div>{props.event.title}</div>
-  //         <img src={'qlevent.png'} alt='Event card' />
-  //         <div>{props.event.description}</div>
-  //         <button onClick={closeModal}>Close</button>
-  //       </div>
-  //     )
-  //   } else {
-  //     return null;
-  //   }
-  // }
 
   return (
 
